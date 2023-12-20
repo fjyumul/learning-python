7dd78179b72cb4eb4b61a2ae07faf2a7c1da3160
Francis Jay Yumul <fpyumul@securitybank.com.ph>
2023-12-20 17:51:38 +0800

    initial commit

--- Ch4 - Dates and Times/calendars_start.py
+++ Ch4 - Dates and Times/calendars_start.py
@@ -5,13 +5,22 @@
 
 
 # TODO: import the calendar module
-
+import calendar
+from datetime import datetime
 
 # TODO: create a plain text calendar
+# year = datetime.now().year
+# month = datetime.now().month
 
+# c = calendar.monthcalendar(year, month)
+# # print(c)
+# print(len([i for i in calendar.monthcalendar(year, month)]))
 
 # TODO: create an HTML formatted calendar
-
+# today = datetime.today()
+# days=["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+# print(today.weekday())
+# print("Tomorrow will be "+days[(today.weekday()+1)])
 
 # TODO: loop over the days of a month
 # zeroes mean that the day of the week is in an overlapping month
@@ -26,3 +35,10 @@
 # To figure out what days that would be for each month,
 # we can use this script:
 
+import calendar
+import datetime
+year = datetime.datetime.now().year
+cal = calendar.TextCalendar(calendar.SUNDAY)
+print(cal.formatmonth(year,1,0,0))
+# for m in range(1,13):
+#     print(cal.formatmonth(year, m, 0, 0))
--- Ch4 - Dates and Times/dates_start.py
+++ Ch4 - Dates and Times/dates_start.py
@@ -3,15 +3,18 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
-
+from datetime import date
+from datetime import time
+from datetime import datetime
 
 def main():
     ## DATE OBJECTS
     # TODO: Get today's date from the simple today() method from the date class
-
+    today = date.today()
+    print ("Today's date is", today)
 
     # TODO: print out the date's individual components
-
+    print("Date components:", today.day, today.month, today.year)
     
     # TODO: retrieve today's weekday (0=Monday, 6=Sunday)
 
